9bb78743818366805441c1210f9ac30817a0082c
devaaaryan <aaaryandev@gmail.com>
2026-01-07 15:47:37 +0530

    Updated code

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="navbar_container">
+                <div className="navbar_logo">
+                    <span>Work</span>ora
+                </div>
+                <ul className='navbar_links'>
+                    <li>Explore</li>
+                    <li>How it Works</li>
+                    <li>Become a Freelancer</li>
+                </ul>
+
+                <div className="navbar_actions">
+                    <button className="btn login">Sign In</button>
+                    <button className="btn signup">Join</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
