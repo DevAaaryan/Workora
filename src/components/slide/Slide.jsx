7014b5add7184efbb94e24cfbd8c048e27fb70f2
devaaaryan <aaaryandev@gmail.com>
2026-01-18 01:26:25 +0530

    Done with Slide component using a lib

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import './Slide.scss'
+import React from "react";
+import "./Slide.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { cards } from "../../data";
+import CatCard from "../catCard/CatCard";
 
-const Slide = () => {
-  return (
-    <div className='slide'>Slide</div>
-  )
+
+const responsive = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5,
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5,
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+    },
+};
+
+const Slide = ({ children }) => {
+    return (
+        <div className='slide'>
+            <div className="container">
+
+                <Carousel responsive={responsive} autoPlay={true} autoPlaySpeed={2000} infinite={true}>
+                    {children}
+                </Carousel>
+            </div>
+        </div>
+    )
 }
 
 export default Slide
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from '../../components/slide/Slide'
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
+
+
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
       <TrustedBy />
+      <Slide>
+        {cards.map(card => {
+          return <CatCard item={card} key={card.id} />
+        })}
+      </Slide>
     </div>
   )
 }
 
 export default Home
+
