cf2904f6efb751770eca6f97d0ce4ed0f17140f9
devaaaryan <aaaryandev@gmail.com>
2026-01-19 00:16:42 +0530

    Updated code of footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -66,12 +66,34 @@ const Footer = () => {
         </div>
         <hr />
         <div className="bottom">
-          <div className="left"></div>
-          <div className="right"></div>
+          <div className="left">
+            <h2>Workora</h2>
+            <span>© Workora International Ltd. 2025</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div><div className="link">
+              <img src="/img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <div>
+              <img src="/img/accessibility.png" alt="" />
+            </div>
+
+          </div>
         </div>
 
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 
