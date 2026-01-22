cad80dfb592c147cacbe390da19b8526f7d34faa
devaaaryan <aaaryandev@gmail.com>
2026-01-22 16:12:47 +0530

    Updated code

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import "./Gigs.scss"
 const Gigs = () => {
   return (
-    <div className='gigs'>Gigs</div>
+    <div className='gigs'>
+      <div className="container">
+        <span className="breadcrumbs">Workora Graphics & Design </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+      </div>
+    </div>
   )
 }
 
