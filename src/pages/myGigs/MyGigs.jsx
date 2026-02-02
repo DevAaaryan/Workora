7c8dad1d2a9fe23a95dedbc2a446f4b85a4b4283
devaaaryan <aaaryandev@gmail.com>
2026-02-02 20:03:25 +0530

    Updated code of My Gigs

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -7,7 +7,9 @@ const MyGigs = () => {
       <div className="container">
         <div className='title'>
           <h1>Gigs</h1>
-          <Link to="/add">Add New gig</Link>
+          <Link to="/add">
+            <button>Add New Gig</button>
+          </Link>
         </div>
 
         <table>
@@ -21,11 +23,87 @@ const MyGigs = () => {
 
           <tr>
             <td>
-              <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <img className='img' src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
             </td>
             <td>Stunning concept art</td>
             <td>59.<sup>99</sup></td>
             <td>13</td>
+            <td>
+              <img className="delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>41</td>
+            <td>
+              <img className="delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>55</td>
+            <td>
+              <img className="delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>119.<sup>99</sup></td>
+            <td>29</td>
+            <td>
+              <img className="delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>59.<sup>99</sup></td>
+            <td>34</td>
+            <td>
+              <img className="delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>110.<sup>99</sup></td>
+            <td>16</td>
             <td>
               <img className="delete" src="./img/delete.png" alt="" />
             </td>
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,113 @@
 import React from 'react'
 import "./Orders.scss"
+import {Link} from 'react-router-dom'
 const Orders = () => {
   return (
-    <div className='orders'>Orders</div>
+    <div className='orders'>
+      <div className="container">
+        <div className='title'>
+          <h1>Orders</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Buyer</th>
+            <th>Contact</th>
+          </tr>
+
+          <tr>
+            <td>
+              <img className='img' src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            </td>
+            <td>Stunning concept art</td>
+            <td>59.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>119.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>59.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>110.<sup>99</sup></td>
+            <td>John koe</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
