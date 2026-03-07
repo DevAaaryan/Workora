cd0d6f47ebcdcd68835fe32ae859a9835f0c185b
devaaaryan <aaaryandev@gmail.com>
2026-03-07 23:49:24 +0530

    Updated code

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -2,8 +2,4 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
-router.post("/logout", logout)
-
 export default router;
